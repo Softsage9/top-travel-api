0449a71c88829a263d5368dbe280f8608252ea6d
Mart Gjepali <gjepalimart@gmail.com>
2024-09-06 13:52:14 +0200

    update

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["X-Total-Count"],
+    expose_headers=["Content-Range", "X-Total-Count"],
 )
 
 
